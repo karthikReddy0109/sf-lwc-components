e7452d5b1c5d0c31ac52dbacebe2b88091db07f0
karthikReddy0109 <rangannagarikarthik1977@gmail.com>
2026-05-27 20:26:11 +0530

    Implemented global search

---
+++ force-app/main/default/lwc/globalSearchComponent/globalSearchComponent.js
@@ -0,0 +1,38 @@
+import { LightningElement } from 'lwc';
+import getSearchResults from '@salesforce/apex/GlobalSearchController.getSearchResults';
+export default class GlobalSearchComponent extends LightningElement {
+    searchTerm;
+    searchTimeout;
+    accounts;
+    contacts;
+    leads;
+    async executeSearch(){
+        try{
+            const searchResults = await getSearchResults({searchTerm : this.searchTerm});
+            this.accounts = searchResults.accounts || [];
+            this.contacts = searchResults.contacts || [];
+            this.leads = searchResults.leads || [];
+        }catch(error){
+            console.log(error);
+        }
+    }
+    handleChange(event){
+        this.searchTerm = event.target.value;
+        clearTimeout(this.searchTimeout);
+        this.searchTimeout = setTimeout(() => {
+            this.executeSearch();
+        }, 300);
+    }
+
+    get hasAccounts(){
+        return this.accounts && this.accounts.length > 0;
+    }
+
+    get hasContacts(){
+        return this.contacts && this.contacts.length > 0;
+    }
+
+    get hasLeads(){
+        return this.leads && this.leads.length > 0;
+    }
+}
